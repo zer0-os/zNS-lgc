3d65252d859e64e096730c4127cf75fe3e4fffe3
Zachary <remscar@gmail.com>
2021-03-18 15:33:34 -0700

    Started to flesh out scenario 1

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -9,6 +9,7 @@ import {
   Registrar,
   Registrar__factory,
 } from "../typechain";
+import { getEvent } from "./helpers";
 
 chai.use(solidity);
 const { expect } = chai;
@@ -25,7 +26,18 @@ describe("End 2 End Tests", () => {
   let user3: SignerWithAddress;
 
   let registrar: Registrar;
+  let registrarAsAdmin: Registrar;
+
+  let registrarAsUser1: Registrar;
+  let registrarAsUser2: Registrar;
+  let registrarAsUser3: Registrar;
+
   let controller: BasicController;
+  let controllerAsAdmin: BasicController;
+
+  let controllerAsUser1: BasicController;
+  let controllerAsUser2: BasicController;
+  let controllerAsUser3: BasicController;
 
   const rootDomain = ethers.constants.HashZero;
 
@@ -44,6 +56,11 @@ describe("End 2 End Tests", () => {
       const registrarFactory = new Registrar__factory(creator);
       registrar = await registrarFactory.deploy();
       await registrar.initialize();
+
+      registrarAsUser1 = registrar.connect(user1);
+      registrarAsUser2 = registrar.connect(user2);
+      registrarAsUser3 = registrar.connect(user3);
+      registrarAsAdmin = registrar.connect(admin);
     });
 
     it("transfers root domain to admin", async () => {
@@ -56,6 +73,11 @@ describe("End 2 End Tests", () => {
       const controllerFactory = new BasicController__factory(controllerCreator);
       controller = await controllerFactory.deploy();
       await controller.initialize(registrar.address);
+
+      controllerAsAdmin = controller.connect(admin);
+      controllerAsUser1 = controller.connect(user1);
+      controllerAsUser2 = controller.connect(user2);
+      controllerAsUser3 = controller.connect(user3);
     });
 
     it("owner of registrar adds controller", async () => {
@@ -66,7 +88,73 @@ describe("End 2 End Tests", () => {
     });
   });
 
-  describe("scenario 1", async () => {
-    // test scenario 1
+  describe("scenario 1", () => {
+    const domain1Name = "user1";
+    let domain1Id: string;
+
+    it("admin creates user1 a domain through controller", async () => {
+      const tx = await controllerAsAdmin.registerDomain(
+        domain1Name,
+        user1.address
+      );
+
+      const event = await getEvent(tx, "RegisteredDomain", controllerAsAdmin);
+
+      domain1Id = event.args["id"];
+
+      expect(await registrar.ownerOf(domain1Id)).to.eq(user1.address);
+    });
+
+    it("user1 transfers domain to user2", async () => {
+      await registrarAsUser1.transferFrom(
+        user1.address,
+        user2.address,
+        domain1Id
+      );
+
+      expect(await registrar.ownerOf(domain1Id)).to.eq(user2.address);
+    });
+
+    it("user2 sets domain content, royalty, and locks it", async () => {
+      const metadataUri = "someuri";
+      let tx = await registrarAsUser2.setDomainMetadataUri(
+        domain1Id,
+        metadataUri
+      );
+      let event = await getEvent(tx, "MetadataChanged", registrarAsUser2);
+
+      expect(event.args["uri"]).to.eq(metadataUri);
+
+      const royaltyAmount = 100;
+      tx = await registrarAsUser2.setDomainRoyaltyAmount(
+        domain1Id,
+        royaltyAmount
+      );
+      event = await getEvent(tx, "RoyaltiesAmountChanged", registrarAsUser2);
+
+      expect(event.args["amount"]).to.eq(royaltyAmount);
+
+      tx = await registrarAsUser2.lockDomainMetadata(domain1Id);
+      expect(await registrarAsUser2.isDomainMetadataLocked(domain1Id)).to.be
+        .true;
+    });
+
+    it("user2 transfers domain back to user1", async () => {
+      await registrarAsUser2.transferFrom(
+        user2.address,
+        user1.address,
+        domain1Id
+      );
+
+      expect(await registrar.ownerOf(domain1Id)).to.eq(user1.address);
+    });
+
+    it("user1 is unable to set metadata or royalty", async () => {
+      let tx = registrarAsUser1.setDomainMetadataUri(domain1Id, "blah");
+      await expect(tx).to.be.revertedWith("Metadata locked");
+
+      tx = registrarAsUser1.setDomainRoyaltyAmount(domain1Id, 0);
+      await expect(tx).to.be.revertedWith("Not locker");
+    });
   });
 });

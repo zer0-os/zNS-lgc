3b03e7fae6abc726471ba1e2124e63aed4c804ff
Christopher Dixon <cryptogrow36@gmail.com>
2021-04-06 10:10:03 -0700

    cleaned up contract logic a bit and added extra functionality to fulfillDomainBid

--- contracts/StakingController.sol
+++ contracts/StakingController.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.3;
 
-import "./@openzeppelin/contracts-upgradeable/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/introspection/ERC165Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721HolderUpgradeable.sol";
 
 
 import "./interfaces/IRegistrar.sol";
@@ -15,26 +15,25 @@ contract StakingController is
   Initializable,
   ContextUpgradeable,
   ERC165Upgradeable,
-  IERC721ReceiverUpgradeable
+  ERC721HolderUpgradeable
 {
 
-  using ECDSA for bytes32;
+  using ECDSAUpgradeable for bytes32;
 
   IERC20Upgradeable private infinity;
   IRegistrar private registrar;
   uint256 private rootDomain;
 
-  mapping(string => address) public approvedBids;
+  mapping(bytes32 => address) public approvedBids;
 
   event DomainBidPlaced(
     bytes32 indexed requestHash,
-    address indexed requester,
-    address indexed domainOwner
+    string indexed domainOwner
   );
 
-  event DomainBidAccepted(uint256 bidIdentifier);
+  event DomainBidAccepted(string indexed bidIdentifier);
 
-  event DomainBidFulfilled(uint256 bidIdentifier);
+  event DomainBidFulfilled(string indexed bidIdentifier);
 
   modifier authorizedOwner(uint256 domain) {
     require(registrar.domainExists(domain), "Invalid Domain");
@@ -43,7 +42,7 @@ contract StakingController is
   }
 
   modifier authorizedRecipient(string memory ipfsHash) {
-    require(approvedBids[ipfsHash] == _msgSender(), "Not Authorized Recipient")
+    require(approvedBids[keccak256(abi.encode(ipfsHash))] == _msgSender(), "Not Authorized Recipient");
     _;
   }
 
@@ -59,65 +58,77 @@ contract StakingController is
     /**
       @notice placeDomainBid allows a user to send a request for a new sub domain to a domains owner
       @param requestHash is the hashed data for a domain request
-      @param domainOwner is the address of the domain parent's owner
       @param ipfsHash is the IPFS hash containing the bids params(ex: name being requested, amount, stc)
+      @dev the IPFS hash must be emitted as a string here for the front end to be able to recover the bid info
     **/
     function placeDomainBid(
       bytes32 requestHash,
-      address domainOwner,
       string memory ipfsHash
     ) external {
-      DomainBidPlaced(
+      emit DomainBidPlaced(
         requestHash,
-        msg.sender,
-        domainOwner,
         ipfsHash
       );
     }
 
     /**
-      @notice Approves a domain bid, allowing the domain to be created.
+      @notice approveDomainBid approves a domain bid, allowing the domain to be created.
       @param parentId is the id number of the parent domain to the sub domain being requested
       @param ipfsHash is the IPFS hash of the bids information
       @param bidder is the address of the account that placed the bid being accepted
     **/
-    function acceptSubRequest(
+    function approveDomainBid(
         uint256 parentId,
         string memory ipfsHash,
         address bidder
     ) external authorizedOwner(parentId) {
-      approvedBids[ipfsHash] = bidder;
+      approvedBids[keccak256(abi.encode(ipfsHash))] = bidder;
       emit DomainBidAccepted(ipfsHash);
     }
 
     /**
       @notice Fulfills a domain bid, creating the domain.
         Transfers tokens from bidders wallet into controller.
-        Will emit a DomainBidFulfilled event.
       @param parentId is the id number of the parent domain to the sub domain being requested
       @param bidAmount is the uint value of the amount of infinity bid
-      @param ipfsHash is the IPFS hash of the bids information
+      @param royaltyAmount is the royalty amount the creator sets for resales on zAuction
+      @param metadata is the IPFS hash of the new domains information
+      @dev this is the same IPFS hash that contains the bids information as this is just stored on its own feild in the metadata
       @param name is the name of the new domain being created
       @param signature is the signature of the bidder
+      @param lockOnCreation is a bool representing whether or not the metadata for this domain is locked
     **/
       function fulfillDomainBid(
         uint256 parentId,
         uint256 bidAmount,
-        string memory ipfsHash,
+        uint256 royaltyAmount,
+        string memory metadata,
         string memory name,
-        bytes memory signature
-      ) external authorizedRecipient(ipfsHash) {
-        address recoveredbidder = recover(keccak256(abi.encode(bidAmount, name, parentId, ipfsHash)), signature);
+        bytes memory signature,
+        bool lockOnCreation
+      ) external authorizedRecipient(metadata) {
+        address recoveredbidder = recover(keccak256(abi.encode(bidAmount, name, parentId, metadata)), signature);
+        address bidder = _msgSender();
+        address controller = address(this);
         require(bidder == recoveredbidder, 'StakingController: incorrect bid info');
-        infinity.transferFrom(bidder, address(this), bidAmount);
-        uint256 id = registrar.registerDomain(parentId, name, msg.sender, registrar.ownerOf(id));
-        emit DomainBidFulfilled(ipfsHash);
+        infinity.transferFrom(bidder, controller, bidAmount);
+        address parentOwner = registrar.ownerOf(parentId);
+        uint256 id = registrar.registerDomain(parentId, name, controller, parentOwner);
+        registrar.setDomainMetadataUri(id, metadata);
+        registrar.setDomainRoyaltyAmount(id, royaltyAmount);
+        registrar.transferFrom(controller, bidder, id);
+
+        if (lockOnCreation) {
+          registrar.lockDomainMetadataForOwner(id);
+        }
+        emit DomainBidFulfilled(metadata);
       }
 
     /**
       @notice recover allows the hashed data of a domain request to be recovered
       @notice requestHash is the hash of the request being recovered
       @notice signature is the signature the hash was created with
+      @dev recover is marked as public because it is used by the fulfillDomainBid function
     **/
     function recover(
       bytes32 requestHash,
@@ -126,15 +137,7 @@ contract StakingController is
     public
     pure
     returns (address) {
-        return hash.recover(signature);
-    }
-
-    /**
-      @notice toEthSignedMessageHash takens in a message hash and signs it for the message sender
-      @param requestHash is the hash of the request message being signed
-    **/
-    function toEthSignedMessageHash(bytes32 requestHash) public pure returns (bytes32) {
-        return hash.toEthSignedMessageHash();
+        return requestHash.recover(signature);
     }
 
   }
--- contracts/interfaces/IStakingController.sol
+++ contracts/interfaces/IStakingController.sol
@@ -7,9 +7,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradea
 interface IStakingController is IERC165Upgradeable, IERC721ReceiverUpgradeable {
   event DomainBidPlaced(
     bytes32 indexed requestHash,
-    address indexed requester,
-    address indexed domainOwner,
-    string ipfsHash
+    bytes32 indexed domainOwner
   );
 
   event DomainBidAccepted(string bidIdentifier);
@@ -17,25 +15,24 @@ interface IStakingController is IERC165Upgradeable, IERC721ReceiverUpgradeable {
   event DomainBidFulfilled(string bidIdentifier);
 
   /**
-    @notice requestDomain allows a user to send a request for a new sub domain to a domains owner
+    @notice placeDomainBid allows a user to send a request for a new sub domain to a domains owner
     @param requestHash is the hashed data for a domain request
-    @param domainOwner is the address of the domain parent's owner
     @param ipfsHash is the IPFS hash containing the bids params(ex: name being requested, amount, stc)
+    @dev the IPFS hash must be emitted as a string here for the front end to be able to recover the bid info
   **/
   function placeDomainBid(
     bytes32 requestHash,
-    address domainOwner,
     string memory ipfsHash
   ) external;
 
   /**
-    @notice Approves a domain bid, allowing the domain to be created.
+    @notice approveDomainBid Approves a domain bid, allowing the domain to be created.
       Will emit a DomainBidAccepted event.
     @param parentId is the id number of the parent domain to the sub domain being requested
     @param ipfsHash is the IPFS hash of the bids information
     @param bidder is the address of the account that placed the bid being accepted
   **/
-  function acceptSubRequest(
+  function approveDomainBid(
     uint256 parentId,
     string memory ipfsHash,
     address bidder
@@ -44,20 +41,24 @@ interface IStakingController is IERC165Upgradeable, IERC721ReceiverUpgradeable {
   /**
     @notice Fulfills a domain bid, creating the domain.
       Transfers tokens from bidders wallet into controller.
-      Will emit a DomainBidFulfilled event.
     @param parentId is the id number of the parent domain to the sub domain being requested
     @param bidAmount is the uint value of the amount of infinity bid
-    @param ipfsHash is the IPFS hash of the bids information
+    @param royaltyAmount is the royalty amount the creator sets for resales on zAuction
+    @param metadata is the IPFS hash of the new domains information
+    @dev this is the same IPFS hash that contains the bids information as this is just stored on its own feild in the metadata
     @param name is the name of the new domain being created
     @param signature is the signature of the bidder
+    @param lockOnCreation is a bool representing whether or not the metadata for this domain is locked
   **/
-  function fulfillDomainBid(
-    uint256 parentId,
-    uint256 bidAmount,
-    string memory ipfsHash,
-    string memory name,
-    bytes memory signature
-  ) external;
+    function fulfillDomainBid(
+      uint256 parentId,
+      uint256 bidAmount,
+      uint256 royaltyAmount,
+      string memory metadata,
+      string memory name,
+      bytes memory signature,
+      bool lockOnCreation
+    ) external;
 
   /**
     @notice recover allows the hashed data of a domain request to be recovered
@@ -65,16 +66,8 @@ interface IStakingController is IERC165Upgradeable, IERC721ReceiverUpgradeable {
     @notice signature is the signature the hash was created with
   **/
   function recover(bytes32 requestHash, bytes memory signature)
-    public
+    external
     pure
     returns (address);
 
-  /**
-    @notice toEthSignedMessageHash takens in a message hash and signs it for the message sender
-    @param requestHash is the hash of the request message being signed
-  **/
-  function toEthSignedMessageHash(bytes32 requestHash)
-    public
-    pure
-    returns (bytes32);
 }

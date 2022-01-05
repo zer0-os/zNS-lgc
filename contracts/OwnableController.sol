2122fc36b945870338433c32acacbb147dba9853
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-01-05 13:46:49 -0800

    Removed OwnableController inheritence of IBasicController

--- contracts/OwnableController.sol
+++ contracts/OwnableController.sol
@@ -12,7 +12,6 @@ import "./interfaces/IBasicController.sol";
 import "./interfaces/IRegistrar.sol";
 
 contract OwnableController is
-  IBasicController,
   ContextUpgradeable,
   OwnableUpgradeable,
   ERC165Upgradeable,

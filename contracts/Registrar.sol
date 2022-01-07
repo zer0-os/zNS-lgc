e78baac489465e4eb509819ad878e7638cb519ca
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-01-07 11:42:38 -0800

    Added new method to burn ERC721 tokens

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -346,4 +346,9 @@ contract Registrar is
 
     emit MetadataLockChanged(id, locker, lockStatus);
   }
+
+  function burnToken(uint256 tokenId) external onlyOwner {
+    transferFrom(ownerOf(tokenId), address(0), tokenId);
+    delete (records[tokenId]);
+  }
 }

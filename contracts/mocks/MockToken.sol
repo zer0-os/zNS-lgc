b054b8a40f4a8fe20532a89d41d87e285d16631e
James Earle <j_earle@hotmail.com>
2022-01-20 14:49:30 -0800

    rinkeby deployment for zns contracts

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -5,14 +5,17 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract MockToken is Initializable, ERC20Upgradeable {
-  function initialize(string name, string symbol) public initializer {
+  function initialize(string calldata name, string calldata symbol)
+    public
+    initializer
+  {
     __ERC20_init(name, symbol);
   }
 
   function mint() public {
     _mint(msg.sender, 10000 * 10**18);
   }
-  
+
   function mintAmountFor(uint256 amount, address forAccount) public {
     _mint(forAccount, amount);
   }

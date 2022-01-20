a553e415c109ce5b4dc32c2e59ad2ebe9887d954
Zachary <remscar@gmail.com>
2022-01-20 10:35:16 -0800

    Update MockToken.sol

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -5,11 +5,15 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract MockToken is Initializable, ERC20Upgradeable {
-  function initialize() public initializer {
-    __ERC20_init("Mock Token", "MT");
+  function initialize(string name, string symbol) public initializer {
+    __ERC20_init(name, symbol);
   }
 
   function mint() public {
     _mint(msg.sender, 10000 * 10**18);
+  }
+  
+  function mintAmountFor(uint256 amount, address for) public {
+    _mint(for, amount);
   }
 }

187f33fc0fe9ff0d4e478f8f308bdfd1cb7cce21
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-02-22 15:55:23 -0800

    Added function to set domain metadata in bulk using an ordered list of IDs and a folder.

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -414,4 +414,42 @@ contract Registrar is
       _setDomainMetadataUri(ids[i], uris[i]);
     }
   }
+
+  function adminSetMetadataBulk(
+    string memory folderWithIPFSPrefix,
+    uint256[] memory orderedIds
+  ) external onlyOwner {
+    for (uint256 i = 0; i < orderedIds.length; i++) {
+      _setDomainMetadataUri(
+        orderedIds[i],
+        string(abi.encodePacked(folderWithIPFSPrefix, uint2str(i)))
+      );
+    }
+  }
+
+  function uint2str(uint256 _i)
+    internal
+    pure
+    returns (string memory _uintAsString)
+  {
+    if (_i == 0) {
+      return "0";
+    }
+    uint256 j = _i;
+    uint256 len;
+    while (j != 0) {
+      len++;
+      j /= 10;
+    }
+    bytes memory bstr = new bytes(len);
+    uint256 k = len;
+    while (_i != 0) {
+      k = k - 1;
+      uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
+      bytes1 b1 = bytes1(temp);
+      bstr[k] = b1;
+      _i /= 10;
+    }
+    return string(bstr);
+  }
 }

2fe09fa4c5548443b42ddeb441cb16e7cd7c65f1
Zachary <remscar@gmail.com>
2022-04-11 18:36:55 -0700

    use script

--- scripts/v1.1/use-zns.ts
+++ scripts/v1.1/use-zns.ts
@@ -1,19 +1,19 @@
 import * as hre from "hardhat";
 import { Registrar__factory } from "../../typechain";
 
-const address = "0x73124d6436a30C998628D980C9c2643aa2021944";
+const address = "0x23682326C87D079F73bd88402efD341E07731aE8";
 
 const main = async () => {
   const signers = await hre.ethers.getSigners();
   const registrar = Registrar__factory.connect(address, signers[0]);
-  const tx = await registrar.registerDomainAndSend(
-    "0x804272242ccaa9b022763d219b2679d0d1067fe5735a0ba9de2a2537afed08d6",
-    "sub3",
-    signers[0].address,
-    "ipfs://QmWdqc8LPzpSTL5jnVhJDUvUAprn425vp4gtKhsaknxShu",
+  const tx = await registrar.registerSubdomainContract(
+    "0x290422f1f79e710c65e3a72fe8dddc0691bb638c865f5061a5e639cf244ee5ed",
+    "wolf",
+    "0xadFe719d34736792956cE8dCa8A325922812BBBE",
+    "ipfs://QmQfEPPMXe5FeqmRoPuQ1P88vJ9nCKGwhF7gFumTB5H25m",
     0,
     true,
-    "0x35888AD3f1C0b39244Bb54746B96Ee84A5d97a53"
+    "0xadFe719d34736792956cE8dCa8A325922812BBBE"
   );
   console.log(tx.hash);
 };

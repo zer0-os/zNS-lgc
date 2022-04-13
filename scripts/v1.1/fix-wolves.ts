c684645591550995c1e33ca91a84562865419456
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-04-13 23:27:39 +0200

    Added script to fix wolf metadata

---
+++ scripts/v1.1/fix-wolves.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+import * as hre from "hardhat";
+import { Registrar__factory } from "../../typechain";
+
+const registrarAddress = "0x1A178CFD768F74b3308cbca9998C767F4E5B2CF8";
+const ipfsFolder = "ipfs://QmfWuoR2Cxezerynjjknhmus1qgGdwLRcBLQVFSrdHAwnJ/";
+const orderedIds: string[] = [];
+
+interface WolfList {
+  ids: string[];
+}
+
+const main = async () => {
+  const signers = await hre.ethers.getSigners();
+  const registrar = Registrar__factory.connect(registrarAddress, signers[0]);
+
+  const wolves = fs.readFileSync("first-900-wolves.json");
+  const wolfContents = JSON.parse(wolves.toString()) as WolfList;
+  const chunkSize = 150;
+  for (let i = 0; i < 900; i += chunkSize) {
+    console.log(`${i}; ${i + chunkSize}`);
+    const currentChunk = wolfContents.ids.slice(i, i + chunkSize);
+    // for (let j = 0; j < chunkSize; j++) {
+    //   console.log(`${j} - ${currentChunk[j]}`);
+    // }
+    const tx = await registrar.adminSetMetadataBulk(
+      ipfsFolder,
+      currentChunk,
+      i
+    );
+    console.log(tx.hash);
+    console.log("awaiting tx...");
+    await tx.wait();
+  }
+};
+
+main().catch(console.error);

149523989fc62ca0891a9e8949cca4d40387d365
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-04-21 10:39:32 -0700

    Removed unnecessary code from read-only script

--- scripts/wolf/generate-wolf-ID-list.ts
+++ scripts/wolf/generate-wolf-ID-list.ts
@@ -1,4 +1,3 @@
-import { BigNumberish } from "ethers";
 import * as hre from "hardhat";
 import { Registrar, Registrar__factory } from "../../typechain";
 import * as fs from "fs";
@@ -6,34 +5,8 @@ import * as fs from "fs";
 const registrarAddress = "0x1A178CFD768F74b3308cbca9998C767F4E5B2CF8"; // "0x1A178CFD768F74b3308cbca9998C767F4E5B2CF8"; // wilder.beasts.wolf reg
 //const registrarAddress = "0xf2f1c79E1b2Ed2B14f3bd577248f9780e50c9BEa"; // Rinkeby jellybeans
 
-const deployerWallet = "0x7829Afa127494Ca8b4ceEF4fb81B78fEE9d0e471"; //for mainnet
-//const deployerWallet = "0x35888AD3f1C0b39244Bb54746B96Ee84A5d97a53"; //rinkeby
-
-const ipfsFolder = "ipfs://QmcCJfBdzuWE11yAJck3f7c7psbev9qGB6dR1C3n8FvEPk/"; //mainnet
-//const ipfsFolder = "ipfs://QmagskkYR1cJjAd2PU7pq68niTY3rhjYQefT1DfGDo6JLL/"; //rinkeby
-
 const main = async () => {
   let deployer = (await hre.ethers.getSigners())[0];
-
-  if ((await hre.ethers.provider.getNetwork()).chainId == 31337) {
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [deployerWallet],
-    });
-
-    deployer = await hre.ethers.getSigner(deployerWallet);
-
-    await hre.network.provider.send("hardhat_setBalance", [
-      deployerWallet,
-      "0x56BC75E2D63100000", // some big number
-    ]);
-  }
-
-  if (deployer.address != deployerWallet) {
-    throw Error(
-      `Deployer address is not the expected address! ${deployer.address} != ${deployerWallet}`
-    );
-  }
 
   const instance: Registrar = Registrar__factory.connect(
     registrarAddress,

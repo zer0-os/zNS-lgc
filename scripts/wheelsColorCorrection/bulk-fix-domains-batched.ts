1d4ae4f92772a66c885669217361d70d32e9ada0
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-05-05 10:01:56 -0700

    Bulk update by index nightmare and bulk update by ID code

--- scripts/wheelsColorCorrection/bulk-fix-domains-batched.ts
+++ scripts/wheelsColorCorrection/bulk-fix-domains-batched.ts
@@ -1,4 +1,5 @@
 import * as hre from "hardhat";
+import * as fs from "fs";
 import { Registrar, Registrar__factory } from "../../typechain";
 
 const registrarAddress = "0xc2e9678a71e50e5aed036e00e9c5caeb1ac5987d"; // "0x1A178CFD768F74b3308cbca9998C767F4E5B2CF8"; // wilder.beasts.wolf reg
@@ -7,8 +8,14 @@ const registrarAddress = "0xc2e9678a71e50e5aed036e00e9c5caeb1ac5987d"; // "0x1A1
 const deployerWallet = "0x7829Afa127494Ca8b4ceEF4fb81B78fEE9d0e471"; //for mainnet
 //const deployerWallet = "0x35888AD3f1C0b39244Bb54746B96Ee84A5d97a53"; //rinkeby
 
-const ipfsFolder = "ipfs://QmSQTLzzsPS67ay4SFKMv9Dq57iSQ7pLWQVUvS3XB5MowK/"; //mainnet // change this
+const ipfsFolder = "ipfs://QmbBVaB2zcJnzqbz6VUipbCk9YPiTZFZ9b28EhxHUox5z7/"; //mainnet
 //const ipfsFolder = "ipfs://QmagskkYR1cJjAd2PU7pq68niTY3rhjYQefT1DfGDo6JLL/"; //rinkeby
+
+const newColoredIDsFile = "newColoredIDs.json";
+
+interface idList {
+  ids: string[];
+}
 
 const main = async () => {
   let deployer = (await hre.ethers.getSigners())[0];
@@ -33,32 +40,33 @@ const main = async () => {
     );
   }
 
+  const input = fs.readFileSync(newColoredIDsFile);
+  const newColoredIDs = JSON.parse(input.toString()) as idList;
+
   const instance: Registrar = Registrar__factory.connect(
     registrarAddress,
     deployer
   );
 
   console.log(`Updating bulk`);
-  const startIndex = 550;
+  const startIndex = 0;
   const endIndex = 1496;
-  const batchSize = 500;
+  const batchSize = 100;
   for (let i = startIndex; i < endIndex; i += batchSize) {
     const trimmedBatchSize = Math.min(batchSize, endIndex - i);
+    const batch = newColoredIDs.ids.slice(i, i + trimmedBatchSize);
 
     console.log(
-      `Updating bulk, startIndex=${startIndex} i=${i} trimmedBatchSize=${trimmedBatchSize}`
+      `Updating bulk, startIndex=${startIndex} i=${i} trimmedBatchSize=${trimmedBatchSize}, batchLength=${batch.length}`
     );
-    const tx = await instance.adminSetMetadataBulkByIndex(
-      ipfsFolder,
-      i + 1, //token start
-      i, //ipfs start
-      trimmedBatchSize //count
-    );
+    console.log(batch);
 
-    console.log(`tx hash: ${tx.hash}`);
-    console.log(`Waiting to be confirmed...`);
-    const res = await tx.wait();
-    console.log(`used ${res.gasUsed.toString()} gas`);
+    // const tx = await instance.adminSetMetadataBulk(ipfsFolder, batch, i);
+
+    // console.log(`tx hash: ${tx.hash}`);
+    // console.log(`Waiting to be confirmed...`);
+    // const res = await tx.wait();
+    // console.log(`used ${res.gasUsed.toString()} gas`);
   }
 };
 

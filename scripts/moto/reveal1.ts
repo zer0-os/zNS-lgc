8719622c7b95ed5ccc1697f8107bcba2640554d0
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-07-22 14:15:58 -0700

    Removed unnecessary args from reveal script

--- scripts/moto/reveal1.ts
+++ scripts/moto/reveal1.ts
@@ -2,13 +2,8 @@ import * as hre from "hardhat";
 import { IRegistrar, IRegistrar__factory } from "../../typechain";
 
 const registrarAddress = "0x51bd5948cf84a1041d2720f56ded5e173396fc95"; // wilder.moto.genesis reg
-const parentDomainId =
-  "0xf972951da6996be9d4fb0a5c7bcbc229c82eb1270af32f05a4fbc57b7d16e3b9"; // wilder.moto.genesis
 
 const deployerWallet = "0xF5A37a4c139D639d04875f1945b59B1fA6cf939B"; // wilder moto deployer
-const minterWallet = "0xEe7Ad892Fdf8d95223d7E94E4fF42E9d0cfeCAFA"; // wheels dao
-
-const numberToMint = 352;
 
 const main = async () => {
   let deployer = (await hre.ethers.getSigners())[0];

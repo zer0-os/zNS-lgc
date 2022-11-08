db6d524911c92cb546d8cbf537c96ef720baff60
beardhero <damienburbine@gmail.com>
2022-11-08 13:00:09 -0700

    OpenSea royalty changes, optimizer runs increased to 20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -74,7 +74,7 @@ const config: HardhatUserConfig = {
           },
           optimizer: {
             enabled: true,
-            runs: 1,
+            runs: 20,
           },
         },
       },

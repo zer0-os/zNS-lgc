70b157d9ddcf2c2c1f8f4da2104c1b9753bf8fd8
James Earle <j_earle@hotmail.com>
2022-12-05 11:41:43 -0800

    script for upgrading and verifying

--- scripts/upgradeRegistrarToHaveFilter.ts
+++ scripts/upgradeRegistrarToHaveFilter.ts
@@ -5,10 +5,6 @@ import {
 } from "../typechain";
 import { getLogger } from "../utilities";
 import { zer0ProtocolAddresses } from "@zero-tech/zero-contracts";
-import {
-  hashBytecodeWithoutMetadata,
-  Manifest,
-} from "@openzeppelin/upgrades-core";
 
 const logger = getLogger("scripts::test-filter");
 
@@ -18,15 +14,17 @@ const main = async () => {
   logger.log(`Using deployer address ${deployer.address}`);
 
   let registrarAddress: string;
+  let beaconAddress: string;
   if (hre.network.name === "hardhat") {
     registrarAddress = "0x009A11617dF427319210e842D6B202f3831e0116";
-    // registrarAddress = "0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D"
+    beaconAddress = "0x9e20B753d87c4B6632394566EcbE7453B5404871";
   } else {
     const addresses = zer0ProtocolAddresses[hre.network.name];
     if (!addresses) {
       throw Error(`Network ${hre.network.name} not supported.`)
     }
     registrarAddress = addresses.zNS.registrar!;
+    beaconAddress = addresses.zNS.subregistrarBeacon!;
   }
 
   logger.log(`Upgrading Registrar at ${registrarAddress} to include OpenSea filter functionality`);
@@ -35,6 +33,7 @@ const main = async () => {
 
   if (hre.network.name === "hardhat") {
     await hre.upgrades.forceImport(registrarAddress, registrarFactory);
+    await hre.upgrades.forceImport(beaconAddress, registrarFactory);
   }
 
   // Factory for registrar that includes of the OS changes
@@ -43,20 +42,34 @@ const main = async () => {
     registrarFactory
   );
 
-  const upgradedRegistrar = await upgradeRegistrarTx.deployed();
-  const implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(upgradedRegistrar.address);
+  await upgradeRegistrarTx.deployed();
 
-  logger.log(`Attempting to verify implementation contract at ${implementationAddress} with etherscan`);
+  logger.log(`Upgrading Registrar Beacon at ${beaconAddress} to include OpenSea filter functionality`);
+
+  // Upgrade Beacon
+  const upgradeRegistrarBeaconTx = await hre.upgrades.upgradeBeacon(beaconAddress, registrarFactory);
+  await upgradeRegistrarBeaconTx.deployed();
+
+  const implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(registrarAddress);
+  const beaconImplementationAddress = await hre.upgrades.beacon.getImplementationAddress(registrarAddress);
+
   try {
+    logger.log(`Attempting to verify implementation contract ${implementationAddress} with etherscan`);
     await hre.run("verify:verify", {
       address: implementationAddress,
       constructorArguments: [],
     });
+    logger.log(`Attempting to verify implementation contract ${beaconImplementationAddress} with etherscan`);
+
+    await hre.run("verify:verify", {
+      address: beaconImplementationAddress,
+      constructorArguments: [],
+    })
   } catch (e) {
     logger.error(`Failed to verify contract: ${e}`);
   }
 
-  logger.log(`Successfully upgraded registrar at ${upgradedRegistrar.address}`);
+  logger.log(`Successfully upgraded registrar at ${registrarAddress}`);
 }
 
 main();
